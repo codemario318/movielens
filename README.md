bc1c0c95401700ad6988a6a88c633f5687dcbf4b
mario <codemario318@gmail.com>
2024-03-27 17:18:56 +0900

    [ADD] 의존성 추가 > r2dbc, postgresql, testcontainers

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## init
 
-[Spring initializer](https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.example&artifactId=demo&name=demo&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.demo&dependencies=webflux)
+[Spring initializer](https://start.spring.io/#!type=gradle-project-kotlin&language=kotlin&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.example&artifactId=demo&name=demo&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.demo&dependencies=data-r2dbc,postgresql,webflux,testcontainers)
+
+```shell
+docker run --rm -ePOSTGRES_PASSWORD=postgres -p5432:5432 --name movielens-postgres postgres:15-alpine
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,20 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.postgresql:r2dbc-postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:r2dbc")
 }
 
 tasks.withType<KotlinCompile> {
